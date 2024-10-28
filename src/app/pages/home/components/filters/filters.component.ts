eb7471d488339411e9ecff2d527aa5a18bbbf3af
Viorel Strogoteanu <viorelss@icloud.com>
2024-10-28 16:07:13 +0100

    Fixed button handler

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -13,13 +13,7 @@ export class FiltersComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onShowCategory(category: string): void {
-    console.log("Category emitted:", category);
+  onChangeCategory(category: string): void {
     this.showCategory.emit(category);
-  }
-
-  onCategorySelect(newCategory: string): void {
-    console.log("Category selected:", newCategory);
-    this.showCategory.emit(newCategory); // Emetti l'evento showCategory
   }
 }
