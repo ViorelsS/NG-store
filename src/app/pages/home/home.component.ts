6a1009f8301d839adc58708c07194821c6ca4ac1
Viorel Strogoteanu <viorelss@icloud.com>
2024-10-28 17:03:19 +0100

    Home component styling completed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 
+const ROWS_HEIGHT: { [id: number]: number } = {
+  1: 400,
+  3: 335,
+  4: 350,
+};
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
 })
 export class HomeComponent implements OnInit {
   cols: number = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
 
   constructor() {}
@@ -14,6 +21,7 @@ export class HomeComponent implements OnInit {
 
   onColumnCountChange(colsNum: number): void {
     this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[colsNum];
   }
 
   onShowCategory(newCategory: string): void {
